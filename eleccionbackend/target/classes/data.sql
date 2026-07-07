1c651ea6e8664b76ae2c10ac58b1052d220f550f
Victor Cadillo <vcadillog@uni.pe>
2026-07-07 10:07:45 -0500

    arreglado lombok

--- eleccionbackend/target/classes/data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO candidato (id_candidato, candidato, partido) VALUES
-    (1, 'Keiko Sofia Fujimori Higuchi', 'Fuerza Popular'),
-    (2, 'Roberto Helbert Sanchez Palomino', 'Juntos por el Perú')
-ON CONFLICT (id_candidato) DO NOTHING;
-
-INSERT INTO resultado_electoral (id_candidato, votos_nacionales, votos_extranjero, total_votos, created_at, updated_at) VALUES
-    (1, 9028008, 195388, 9223396, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-    (2, 9060022, 113733, 9173755, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
-ON CONFLICT (id_resultado) DO NOTHING;
